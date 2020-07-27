bbf7563e9b57bf30c7cc1ce6a319f130d9d00e57
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-27 05:48:41 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,6 +7,13 @@ src = [
   [99, 50, 3, 4],
   [23, 41]
   ]
-  
 
+row_index = 0
+while row_index < src.count do
+  element_index = 0
+  while element_index < scr[row_index].count do
+    p src[row_index][element_index]
+    element_index += 1
+  end
+  row_index += 1
 end
